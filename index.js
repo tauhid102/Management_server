9b92ea7692eaffb7ee2e32fc05c970afb1168acc
tauhid102 <tauhidurr102@gmail.com>
2022-01-06 21:32:55 +0600

    server add product

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ async function run() {
         const database = client.db('bookShop');
         const booksCollection = database.collection('books');
         const purchasedCollection = database.collection('purchased');
+        const reviewsCollection = database.collection('reviews');
+        const usersCollection = database.collection('users');
 
         app.get('/books', async (req, res) => {
             const cursor = booksCollection.find({});
@@ -41,6 +43,50 @@ async function run() {
             const result = await booksCollection.findOne(query);
             console.log('load', result);
             res.send(result);
+        });
+        //find order using email
+        app.get('/purchased', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const cursor = purchasedCollection.find(query);
+            const buy = await cursor.toArray();
+            res.json(buy);
+        });
+        //cancel order
+        app.delete('/purchased/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await purchasedCollection.deleteOne(query);
+            res.json(result);
+        });
+        //set review
+        app.post('/reviews', async (req, res) => {
+            const cursor = req.body;
+            const result = await reviewsCollection.insertOne(cursor);
+            res.json(result);
+        });
+        //set user in database
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+        //find admin role
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email }
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
+        //add product
+        app.post('/books', async (req, res) => {
+            const cursor = req.body;
+            const result = await booksCollection.insertOne(cursor);
+            res.json(result);
         });
     }
     finally {
